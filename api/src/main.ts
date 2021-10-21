60f9cde9b38ad9781344795406fddb520c1575bc
Christopher Nethercott <ccnethercott@gmail.com>
2021-10-21 11:19:04 +0100

    feat(api): Use helmet middlewear. Closes #8

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import helmet, * from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Define middlewares
+  app.use(helmet());
+
   await app.listen(3000);
 }
 
