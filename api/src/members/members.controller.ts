e3edf5c3aa94dddd276d7ee0db36be64fa87146c
Christopher Nethercott <ccnethercott@gmail.com>
2021-10-22 16:03:34 +0100

    feat(api/member): create simple GET route

--- api/src/members/members.controller.ts
+++ api/src/members/members.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('members')
-export class MembersController {}
+export class MembersController {
+    @Get()
+    getRoot(): string {
+        return 'Will return a list of members';
+    }
+
+    @Get("member/:id")
+    getMember(@Param('id') id: string): string {
+        return `You requested a member with ID ${id}`
+    }
+}
