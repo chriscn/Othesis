e9efc6a948bca6040748437280449f2dfaf3f23d
Christopher Nethercott <ccnethercott@gmail.com>
2021-10-22 16:30:55 +0100

    feat(api/testing): wrriten four tests for the route of the project

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -7,14 +8,31 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
+      providers: [AppService]
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('root GET', () => {
+    it('should return the project name', () => {
+      expect(appController.getRoot()['name']).toBe('othesis-api');
     });
+
+    it('should return a project version of correct type', () => {
+      expect(appController.getRoot()['version']).toMatch(/[0-9].[0-9].[0-9]$/);
+    });
+
+    it('should not return an empty description', () => {
+      expect(appController.getRoot()['description']).not.toBeNull();
+    });
+
+    it('should have Christopher Nethercott as the author', () => {
+      expect(appController.getRoot()['author']).toBe('Christopher Nethercott');
+    });
+
+    it('should return the AGPL-3.0-only license', () => {
+      expect(appController.getRoot()['license']).toBe('AGPL-3.0-only');
+    })
   });
 });
